940b0beae7d7bf50b57aac2de7d3691179c5a877
Kaique Lopes <91297462+oKaiquelopes@users.noreply.github.com>
2024-04-22 16:18:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function toggleDropdown() {
     var dropdownContent = document.getElementById("myDropdown");
     dropdownContent.classList.toggle("show");
 }
-  // Fechar o menu dropdown se o usuário clicar fora dele
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -26,15 +25,53 @@ function toggleDropdown() {
     }
 };
 
-// Obter todos os itens da lista
 const benefitsItems = document.querySelectorAll('.benefits-list li');
 
-// Adicionar evento de clique a cada item da lista
 benefitsItems.forEach(item => {
   const title = item.querySelector('.benefit-title');
 
   title.addEventListener('click', () => {
-    // Alternar a classe 'active' no item clicado (para expandi-lo ou fechá-lo)
     item.classList.toggle('active');
   });
 });
+
+const text = "Descubra o PowerPro: Desenvolvimento de Aplicativos Simplificado para Todos.";
+const textElement = document.getElementById("home-title");
+
+let index = 0;
+
+function type() {
+  if (index < text.length) {
+    textElement.textContent += text[index];
+    index++;
+  } else {
+    // Quando o texto estiver completamente digitado, adicione a barra piscante
+    textElement.innerHTML += '<span id="blinking-cursor">|</span>';
+    clearInterval(typingInterval);
+    setInterval(blinkCursor, 500); // Inicia a função para fazer a barra piscar
+  }
+}
+
+function blinkCursor() {
+  const cursor = document.getElementById("blinking-cursor");
+  cursor.style.visibility = (cursor.style.visibility === 'hidden') ? 'visible' : 'hidden';
+}
+
+const typingInterval = setInterval(type, 90);
+
+
+
+
+const autorizarCheckbox = document.getElementById('autorizarCheckbox');
+const meuBotao = document.getElementById('meuBotao');
+
+meuBotao.addEventListener('click', function() {
+  if (autorizarCheckbox.checked) {
+    // Se o checkbox estiver marcado, define o caminho do arquivo relativo ao href do botão
+    meuBotao.href = 'PowerProSitev2/PowerPro.rar.zip';
+  } else {
+    alert('Por favor, autorize clicando na caixa de seleção antes de prosseguir.');
+  }
+});
+
+
