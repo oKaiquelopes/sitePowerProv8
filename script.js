92db50c9e55e30a2b4151fbecfdb06a20aeff323
Kaique Lopes <91297462+oKaiquelopes@users.noreply.github.com>
2024-05-03 23:20:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -93,3 +93,15 @@ const toggleModal = () => {
 });
 
 
+var msgCookies = document.getElementById('cookies-msg')
+
+function aceito() {
+  localStorage.lgpd = 'sim'
+  msgCookies.classList.remove('mostrar')
+}
+
+if(localStorage.lgpd == 'sim') {
+  msgCookies.classList.remove('mostrar')
+}else{
+  msgCookies.classList.add('mostrar')
+}
